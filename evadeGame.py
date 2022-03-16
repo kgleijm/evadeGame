f9d29f6e0899a6314be1d978808aa94f25c17213
kevin <kgleijm@gmail.com>
2022-03-16 23:12:28 +0100

    Fix algorithm, there was a bug having the players reset over and over again. Now the fitness actually converges

--- evadeGame.py
+++ evadeGame.py
@@ -1,4 +1,5 @@
 import copy
+import random
 import random as r
 from time import sleep
 from typing import Tuple
@@ -27,28 +28,70 @@ class SimpleBlockRenderer:
 
 class Player:
     STAY, LEFT, RIGHT = 0, 1, 2
+    IDCount = 0
+    FitnessVarianceLog = [0.001]
 
     def __init__(self):
         self.movementGrid = None
+        self.ID = -1
+        self.getNewID()
+        self.mutationCount = 0
+        self.fitnessLog = []
+        self.age = 0
+        self.isSetup = False
+
+    def reset(self):
+        self.getNewID()
+        self.mutationCount = 0
+        self.fitnessLog = []
+        self.age = 0
 
     def getRandomValue(self):
         return r.random() * 2 - 1
 
     def setup(self, playingfield):
-        self.movementGrid = [[self.getRandomValue() for j in range(len(playingfield[0]))] for i in
-                             range(len(playingfield))]
-        #print(self.movementGrid)
+        if not self.isSetup:
+            self.movementGrid = [[self.getRandomValue() for j in range(len(playingfield[0]))] for i in range(len(playingfield))]
+            self.isSetup = True
 
     def choose(self, gameState):
         dir = 0
         for y in range(len(gameState)):
-            for x in range(len(gameState)):
+            for x in range(len(gameState[0])):
                 if gameState[y][x] == 1:
                     dir += self.movementGrid[y][x]
         if dir < 0:
             return Player.LEFT
         else:
             return Player.RIGHT
+
+    def mutate(self, chance):
+        for y in range(len(self.movementGrid)):
+            for x in range(len(self.movementGrid[0])):
+                if r.random() < chance:
+                    self.movementGrid[y][x] = self.getRandomValue()
+                    self.mutationCount += 1
+
+    def getNewID(self):
+        self.ID = Player.IDCount
+        Player.IDCount += 1
+
+    def logFitness(self, fitness):
+        if fitness not in self.fitnessLog:
+            self.fitnessLog.append(fitness)
+
+    def getFitnessVariance(self):
+        try:
+            variance = (max(self.fitnessLog) - min(self.fitnessLog))
+            Player.FitnessVarianceLog.append(variance)
+            return variance
+        except:
+            return 0.0
+
+
+    def __repr__(self):
+        return f"Player(ID: {self.ID}, Mutations: {self.mutationCount}, fitnessVariance: {'{:.3f}'.format(self.getFitnessVariance())}, age: {self.age} )"
+
 
 
 class EvadeGame:
@@ -74,6 +117,7 @@ class EvadeGame:
         self.ticks = 0
         self.playerCoordinate = {"x": int(width / 2), "y": height - 1}
         self.shouldRender = render
+
 
     def run(self):
         self.running = True
@@ -124,19 +168,28 @@ class EvadeGame:
 
 
 class GeneticAlgorithm:
-    def __init__(self, populationSize=10, stopFitness=100, crossOverChance=0.8, mutationChance=0.01, crossOverPercentage=0.5, amountOfGamesToDecideAverage=20):
+    def __init__(self, populationSize=10, stopFitness=100, crossOverChance=0.8, mutationChance=0.01, crossOverPercentage=0.5, amountOfGamesToDecideAverage=20, topXIndividualsConsidered=6, elitism=0):
         self.populationSize = populationSize
         self.stopFitness = stopFitness
         self.crossOverChance = crossOverChance
         self.mutationChance = mutationChance
         self.amountOfGamesToDecideAverage = amountOfGamesToDecideAverage
-        self.population = [(Player(), 0.0) for _ in range(populationSize)]
+        self.topXIndividualsConsidered = topXIndividualsConsidered
+        self.elitism = elitism
+        self.population = []
+        for _ in range(populationSize):
+            p = Player()
+            self.population.append((p, self.determineFitness(p)))
+
+
         self.crossOverPercentage = crossOverPercentage
 
     def crossOverPlayers(self, parentA: Player, parentB: Player) -> Tuple[Player, Player]:
         childA, childB = copy.deepcopy(parentA), copy.deepcopy(parentB)
-        for y in range(parentA.movementGrid):
-            for x in range(parentA.movementGrid[0]):
+        childA.reset()
+        childB.reset()
+        for y in range(len(parentA.movementGrid)):
+            for x in range(len(parentA.movementGrid[0])):
                 if r.random() < self.crossOverPercentage:
                     childA.movementGrid[y][x] = parentB.movementGrid[y][x]
                     childB.movementGrid[y][x] = parentA.movementGrid[y][x]
@@ -144,24 +197,68 @@ class GeneticAlgorithm:
 
     def determineFitness(self, player: Player) -> float:
         results = []
+        player.age += 1
         for i in range(self.amountOfGamesToDecideAverage):
             result = EvadeGame(11, 8, player, render=False).run()
             results.append(result)
-        return sum(results)/len(results)
+        fitness = sum(results)/len(results)
+        player.logFitness(fitness)
+        return fitness
 
     def getAverageFitness(self) -> float:
         return sum([player[1] for player in self.population])/len(self.population)
 
+    def stripFitness(self, players):
+        return [player[0] for player in players]
+
+    def printSortedPopulation(self, pop):
+        for e in sorted(pop, key=lambda x: x[1], reverse=True):
+            print(e)
+        print("")
+
     def tick(self):
-        self.population = [(player[0], self.determineFitness(player[0])) for player in self.population]
-        print(f"Average fitness of population = {self.getAverageFitness()}")
+        oldPopulation = sorted(self.population, key=lambda x: x[1], reverse=True)
+        print("oldPopulation")
+        self.printSortedPopulation(oldPopulation)
+
+
+        newPopulation = []
+
+        elites = []
+        if self.elitism > 0:
+            elites = self.stripFitness(oldPopulation[:self.elitism])
+            newPopulation += elites
+
+        while len(newPopulation) < self.populationSize:
+            parents = random.sample(oldPopulation[:self.topXIndividualsConsidered], k=2)
+            if r.random() < self.crossOverChance:
+                newPopulation += self.crossOverPlayers(parents[0][0], parents[1][0])
+            else:
+                newPopulation += [parents[0][0], parents[1][0]]
+
+        for player in newPopulation:
+            if player not in elites:
+                player.mutate(self.mutationChance)
+
+        fittedPopulation = [(player, self.determineFitness(player)) for player in newPopulation]
+
+        print("newFittedPopulation")
+        self.printSortedPopulation(fittedPopulation)
+
+        self.population = fittedPopulation
+
+
+
+
+        print(f"Average fitness of population = {self.getAverageFitness()}\n"
+              f"Average variance = {(sum(Player.FitnessVarianceLog))/len(Player.FitnessVarianceLog)} over {len(Player.FitnessVarianceLog)} entries \n\n")
 
     def run(self):
         while self.getAverageFitness() < self.stopFitness:
             self.tick()
 
 
-geneticAlgorithm = GeneticAlgorithm()
+geneticAlgorithm = GeneticAlgorithm(populationSize=10, elitism=2, mutationChance=0.005, crossOverChance=0.8, amountOfGamesToDecideAverage=100, topXIndividualsConsidered=6)
 geneticAlgorithm.run()
 
 
